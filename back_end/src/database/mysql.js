4b3173285c6058b5b56452e9290c697e3be401db
ngoctan3006 <ngoctana1k2014@gmail.com>
2021-12-04 02:38:48 +0700

    update database

--- back_end/src/database/mysql.js
+++ back_end/src/database/mysql.js
@@ -1,15 +1,15 @@
 import mysql from 'mysql'
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Tan30062000',
-  database: 'happychildren',
+    host: 'localhost',
+    user: 'root',
+    password: 'Tan30062000',
+    database: 'happychildren',
 })
 
 // connect to database
-connection.connect(function (err) {
-  if (err) throw err
+connection.connect(err => {
+    if (err) throw err
 })
 
 export default connection
