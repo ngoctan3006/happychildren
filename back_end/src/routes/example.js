3f24d2a02f2d60d35abfcb41e89546843b3c7762
ngoctan3006 <ngoctana1k2014@gmail.com>
2021-12-04 02:40:15 +0700

    delete example api

--- back_end/src/routes/example.js
+++
@@ -1,7 +0,0 @@
-export default function example(req, res, next) {
-    res.status(200).send({
-        message: 'You request GET /example',
-        body: req.body,
-        query: req.query
-    })
-}
