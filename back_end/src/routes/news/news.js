897a38352254cfc65b8d126f834ad0fa1fc60097
ngoctan3006 <ngoctana1k2014@gmail.com>
2021-12-21 09:19:11 +0700

    update news api

--- back_end/src/routes/news/news.js
+++ back_end/src/routes/news/news.js
@@ -6,56 +6,56 @@ const news = express.Router()
 news.get('/', (req, res) => {
     const category = req.query.category
     if(category) {
-        connection.query('select * from happychildren.post_news where deletedAt is null and category = ? order by updatedAt desc', [category], (err, result) => {
+        connection.query('select * from happychildren.post_news where deletedAt is null and category = ? order by updatedAt desc', [category], (err, results) => {
             if(err) {
                 res.status(404).send({
                     message: err
                 })
             } else {
-                res.status(200).send(result)
+                res.status(200).send(results)
             }
         })
     } else {
-        connection.query('select * from happychildren.post_news where deletedAt is null order by updatedAt desc', (err, result) => {
+        connection.query('select * from happychildren.post_news where deletedAt is null order by updatedAt desc', (err, results) => {
             if(err) {
                 res.status(404).send({
                     message: err
                 })
             } else {
-                res.status(200).send(result)
+                res.status(200).send(results)
             }
         })
     }
 })
 
 news.get('/trash', (req, res) => {
-    connection.query('select * from happychildren.post_news where deletedAt is not null order by updatedAt desc', (err, result) => {
+    connection.query('select * from happychildren.post_news where deletedAt is not null order by updatedAt desc', (err, results) => {
         if(err) {
             res.status(404).send({
                 message: err
             })
         } else {
-            res.status(200).send(result)
+            res.status(200).send(results)
         }
     })
 })
 
 news.get('/:id', (req, res) => {
     const id = req.params.id
-    connection.query('select * from happychildren.post_news where id = ?', [id], (err, result) => {
+    connection.query('select * from happychildren.post_news where id = ?', [id], (err, results) => {
         if(err) {
             res.status(404).send({
                 message: err
             })
         } else {
-            res.status(200).send(result)
+            res.status(200).send(results)
         }
     })
 })
 
 news.post('/create', (req, res) => {
     const data = req.body
-    connection.query('insert into happychildren.post_news (title, content, category) values (?, ?, ?)', [data.title, data.content, data.category], (err, result) => {
+    connection.query('insert into happychildren.post_news (title, content, category) values (?, ?, ?)', [data.title, data.content, data.category], (err, results) => {
         if(err) {
             res.status(404).send({
                 message: err
@@ -69,7 +69,7 @@ news.post('/create', (req, res) => {
 news.patch('/edit/:id', (req, res) => {
     const id = req.params.id
     const data = req.body
-    connection.query('update happychildren.post_news set title = ?, content = ?, category = ? where id = ?', [data.title, data.content, data.category, id], (err, result) => {
+    connection.query('update happychildren.post_news set title = ?, content = ?, category = ? where id = ?', [data.title, data.content, data.category, id], (err, results) => {
         if(err) {
             res.status(404).send({
                 message: err
@@ -82,7 +82,7 @@ news.patch('/edit/:id', (req, res) => {
 
 news.patch('/delete/:id', (req, res) => {
     const id = req.params.id
-    connection.query('update happychildren.post_news set deletedAt = current_timestamp() where id = ?', [id], (err, result) => {
+    connection.query('update happychildren.post_news set deletedAt = current_timestamp() where id = ?', [id], (err, results) => {
         if(err) {
             res.status(404).send({
                 message: err
@@ -95,7 +95,7 @@ news.patch('/delete/:id', (req, res) => {
 
 news.patch('/restore/:id', (req, res) => {
     const id = req.params.id
-    connection.query('update happychildren.post_news set deletedAt = null where id = ?', [id], (err, result) => {
+    connection.query('update happychildren.post_news set deletedAt = null where id = ?', [id], (err, results) => {
         if(err) {
             res.status(404).send({
                 message: err
@@ -108,7 +108,7 @@ news.patch('/restore/:id', (req, res) => {
 
 news.post('/hdelete/:id', (req, res) => {
     const id = req.params.id
-    connection.query('delete from happychildren.post_news where id = ?', [id], (err, result) => {
+    connection.query('delete from happychildren.post_news where id = ?', [id], (err, results) => {
         if(err) {
             res.status(404).send({
                 message: err
