048516c7616e917e5a4fae086cc7ff4dc2c5f4c2
ngoctan3006 <ngoctana1k2014@gmail.com>
2021-12-21 09:20:04 +0700

    update app

--- back_end/src/routes/index.js
+++ back_end/src/routes/index.js
@@ -1,6 +1,17 @@
-// export { default as centerRoutes } from './center'
-// export { default as exchangeRoutes } from './exchange'
-export { default as newsRoutes } from './news'
-export { default as productsRoutes } from './products'
-export { default as transactionRoutes } from './transaction'
-export { default as userRoutes } from './users'
+import userRoutes from './users'
+import newsRoutes from './news'
+import transactionRoutes from './transaction'
+import productsRoutes from './products'
+// import centerRoutes  from './center'
+// import exchangeRoutes  from './exchange'
+
+export default (app) => {
+    app.use('/api/v1/users', userRoutes)
+    app.use('/api/v1/news', newsRoutes)
+    app.use('/api/v1/transaction', transactionRoutes)
+    app.use('/api/v1/products', productsRoutes)
+
+    app.use('/', (req, res) => {
+        res.send('<h1>Hello from homepage</h1>')
+    })
+}
