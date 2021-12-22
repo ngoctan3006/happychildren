62c4ed1ce3ca8386ac2c6ae398c441d560f48c76
ngoctan3006 <ngoctana1k2014@gmail.com>
2021-12-22 15:16:45 +0700

    add api to front

--- back_end/src/routes/index.js
+++ back_end/src/routes/index.js
@@ -5,13 +5,13 @@ import productsRoutes from './products'
 // import centerRoutes  from './center'
 // import exchangeRoutes  from './exchange'
 
-export default (app) => {
+export default app => {
     app.use('/api/v1/users', userRoutes)
     app.use('/api/v1/news', newsRoutes)
     app.use('/api/v1/transaction', transactionRoutes)
     app.use('/api/v1/products', productsRoutes)
 
     app.use('/', (req, res) => {
-        res.send('<h1>Hello from homepage</h1>')
+        res.send('<h1>Vào đây làm cái gì =)))</h1>')
     })
 }
